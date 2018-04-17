81696806a4388343c3da2f465f380d84cb4bc975
Maksimys87 <maksimys87051@gmail.com>
2018-04-17 17:47:17 +0300

    Added task3605

---
+++ 4.JavaCollections/src/com/javarush/task/task36/task3605/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task36.task3605;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.TreeSet;
+
+/* 
+Использование TreeSet
+*/
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        TreeSet<Character> treeSet = new TreeSet<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(args[0]))) {
+            while (reader.ready()) {
+                String line = reader.readLine().toLowerCase();
+                char[] chars = line.toCharArray();
+                for (char c : chars) {
+                    if (c > 96) treeSet.add(c);
+                }
+            }
+        }
+
+        Iterator<Character> iterator = treeSet.iterator();
+        for (int i = 0; iterator.hasNext(); i++) {
+            if (i > 4) return;
+            System.out.print(iterator.next());
+        }
+    }
+}
