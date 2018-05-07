e13f2cc34d9b3365cdebc68dcc5df855e36e403e
Maksimys87 <maksimys87051@gmail.com>
2018-05-07 22:44:01 +0300

    Added task3708

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3708/retrievers/CachingProxyRetriever.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task37.task3708.retrievers;
+
+import com.javarush.task.task37.task3708.cache.LRUCache;
+import com.javarush.task.task37.task3708.storage.Storage;
+
+public class CachingProxyRetriever implements Retriever {
+    private LRUCache lruCache = new LRUCache(10);
+    private OriginalRetriever originalRetriever;
+
+    public CachingProxyRetriever(Storage storage) {
+        this.originalRetriever = new OriginalRetriever(storage);
+    }
+
+    @Override
+    public Object retrieve(long id) {
+        if (lruCache.find(id) == null) {
+            Object o = originalRetriever.retrieve(id);
+            lruCache.set(id, o);
+            return o;
+        }
+        return lruCache.find(id);
+    }
+}
